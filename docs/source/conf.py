253e4cefc7ad0b4f901a1f29da19bc725ad1244c
Tammie <tammied68@yahoo.com>
2026-06-02 22:21:14 -0500

    Fix Sphinx Django configuration and module documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,26 +3,35 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+import django
 
-project = 'News Management System'
-copyright = '2026, Tammie Davis'
-author = 'Tammie Davis'
-release = '1.0'
+# Allow Sphinx to import Django project modules from the project root.
+sys.path.insert(0, os.path.abspath("../.."))
+
+# Configure Django settings before autodoc imports project modules.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "news_project.settings")
+django.setup()
+
+# -- Project information -----------------------------------------------------
+
+project = "News Management System"
+copyright = "2026, Tammie Davis"
+author = "Tammie Davis"
+release = "1.0"
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
-
-
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
